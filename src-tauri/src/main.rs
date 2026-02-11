1ffbefa0bff68b81f6f1c8891a2829d6251560c6
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-02-11 19:44:13 +0800

    feat: all files

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,18 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod pyvm;
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            pyvm::list_installed,
+            pyvm::list_available,
+            pyvm::install,
+            pyvm::use_version,
+            // pyvm::current_version
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+    appdemo_lib::run()
+}
